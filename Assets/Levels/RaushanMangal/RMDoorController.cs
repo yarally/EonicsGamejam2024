8d3cf1dfb4744fa3db7a5c7f830079ede9cb6607
Raushan2711 <90211405+Raushan2711@users.noreply.github.com>
2024-03-30 23:12:13 +0100

    Add Level Stairway from Hell

---
+++ Assets/Levels/RaushanMangal/RMDoorController.cs
@@ -0,0 +1,12 @@
+using Lib.Door;
+
+namespace Levels.RaushanMangal
+{
+    public class DoorController : AbstractDoorController
+    {
+        protected override bool CanOpen()
+        {
+            return base.CanOpen();
+        }
+    }
+}
