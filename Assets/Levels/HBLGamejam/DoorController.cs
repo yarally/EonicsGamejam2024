d97b97a4c1426b0a48e1c2a68f553b548d3bf0a8
DerEchteGhanzzz <79211646+DerEchteGhanzzz@users.noreply.github.com>
2024-04-03 22:26:19 +0200

    Add files via upload

---
+++ Assets/Levels/HBLGamejam/DoorController.cs
@@ -0,0 +1,12 @@
+using Lib.Door;
+
+namespace Levels.HBLGamejam
+{
+    public class DoorController : AbstractDoorController
+    {
+        protected override bool CanOpen()
+        {
+            return base.CanOpen();
+        }
+    }
+}
